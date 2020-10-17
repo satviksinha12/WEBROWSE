f88711dae3020f43de0ca456b092f04e3e3d8bcc
satviksinha12 <Sinhasatvik214@Gmail.com>
2020-10-17 15:03:36 +0530

    fixed bug

---
+++ app/src/androidTest/java/com/satvik/webrowse/MainActivityTest2.java
@@ -0,0 +1,124 @@
+package com.satvik.webrowse;
+
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.filters.LargeTest;
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.runner.AndroidJUnit4;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest2 {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void mainActivityTest2() {
+        // Added a sleep statement to match the app's execution delay.
+        // The recommended way to handle such scenarios is to use Espresso idling resources:
+        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+        try {
+            Thread.sleep(7000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        ViewInteraction switch_ = onView(
+                allOf(withId(R.id.switch1), withText("Duckduckgo"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                6),
+                        isDisplayed()));
+        switch_.perform(click());
+
+        ViewInteraction switch_2 = onView(
+                allOf(withId(R.id.switch2), withText("Google"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                7),
+                        isDisplayed()));
+        switch_2.perform(click());
+
+        ViewInteraction switch_3 = onView(
+                allOf(withId(R.id.switch1), withText("Duckduckgo"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                6),
+                        isDisplayed()));
+        switch_3.perform(click());
+
+        ViewInteraction switch_4 = onView(
+                allOf(withId(R.id.switch2), withText("Google"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                7),
+                        isDisplayed()));
+        switch_4.perform(click());
+
+        ViewInteraction appCompatImageButton = onView(
+                allOf(withId(R.id.home),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                4),
+                        isDisplayed()));
+        appCompatImageButton.perform(click());
+
+        ViewInteraction appCompatImageButton2 = onView(
+                allOf(withId(R.id.refresh),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                5),
+                        isDisplayed()));
+        appCompatImageButton2.perform(click());
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+}
---
+++ app/src/main/java/com/satvik/webrowse/MainActivity.java
@@ -0,0 +1,74 @@
+package com.satvik.webrowse;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ImageButton;
+import android.widget.Switch;
+
+import com.google.android.material.switchmaterial.SwitchMaterial;
+import com.google.firebase.analytics.FirebaseAnalytics;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAnalytics mFirebaseAnalytics;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
+        ImageButton back = findViewById(R.id.back);
+        ImageButton forward = findViewById(R.id.forward);
+        ImageButton home = findViewById(R.id.home);
+        ImageButton refresh = findViewById(R.id.refresh);
+        SwitchMaterial duckduckgo = findViewById(R.id.switch2);
+        SwitchMaterial google = findViewById(R.id.switch1);
+        final WebView mywebview = findViewById(R.id.webView);
+        mywebview.setWebViewClient(new WebViewClient());
+        mywebview.loadUrl("https://www.google.com");
+        mywebview.getSettings().setJavaScriptEnabled(true);
+        mywebview.getSettings().setSupportZoom(true);
+        mywebview.clearSslPreferences();
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mywebview.goBack();
+            }
+        });
+        forward.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mywebview.goForward();
+            }
+        });
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mywebview.loadUrl("https://www.google.com");
+            }
+        });
+        refresh.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mywebview.reload();
+            }
+        });
+        duckduckgo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mywebview.loadUrl("https://www.duckduckgo.com");
+            }
+        });
+        google.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mywebview.loadUrl("https://www.google.com");
+            }
+        });
+
+
+    }
+}
