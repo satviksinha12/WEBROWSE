69980d5297e1015ed22e3a60340967c73640c661
satviksinha12 <Sinhasatvik214@Gmail.com>
2020-10-18 20:51:00 +0530

    Added setting page , bookmarks page and Downloads page

--- app/src/main/java/com/satvik/webrowse/MainActivity2.java
+++ app/src/main/java/com/satvik/webrowse/MainActivity2.java
@@ -2,7 +2,10 @@ package com.satvik.webrowse;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -10,5 +13,21 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        Button book = findViewById(R.id.Bookmark);
+        Button down = findViewById(R.id.Downloads);
+        book.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent j = new Intent(MainActivity2.this,Bookmark.class);
+                startActivity(j);
+            }
+        });
+        down.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent k = new Intent(MainActivity2.this,Downloads.class);
+                startActivity(k);
+            }
+        });
     }
 }
